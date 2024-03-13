d0b946c54510274063eee1453580bc6fd5dd09a2
DESKTOP-G085NET\csi23-nrojlla <csi23-nrojlla@colegioaltair.net>
2024-03-13 11:17:28 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConvertirTextoAsterisco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe un texto");
+            string frase = Console.ReadLine();
+            Console.WriteLine("Escribe el caracter a cambiar");
+            char caracterCambio = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Escribe el caracter por que cambia:");
+            char caracterPorCambiar = Convert.ToChar(Console.ReadLine());
+
+            frase.Replace(caracterCambio, caracterPorCambiar);
+            Console.WriteLine(frase.Replace(caracterCambio , caracterPorCambiar));
+        }
+    }
+}
